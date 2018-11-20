b801dd89fd992405c3e87a5a292d0424648b309b
Luvuyo Sono <luvuyosono@gmail.com>
2018-11-20 18:05:23 +0200

    server setup

---
+++ server/services/authService.js
@@ -0,0 +1,14 @@
+module.exports = function(pool, github) {
+    async function register(user) {
+        await po
+    }
+
+    async function login(user) {
+
+    }
+
+    return {
+        register,
+        login
+    }
+}
