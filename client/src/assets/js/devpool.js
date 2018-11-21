a4a3a50b1086e664871254f594b72765fc959c19
Your Name <amandagxagxa@gmail.com>
2018-11-21 10:58:36 +0200

    vue client setup

---
+++ client/src/assets/js/devpool.js
@@ -0,0 +1,15 @@
+
+let devPool = new Vue({
+  el: '.devPool',
+   data : {     
+    // add this
+    username: '',
+    password: '',
+    checked :''
+  },
+  methods : {
+    details : function() {
+      alert('It happenning!!!');
+    }
+  }
+});
