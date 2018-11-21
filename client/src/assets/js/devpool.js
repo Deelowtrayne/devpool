352813168a2c3e574c1778e048510d236b8892f4
Your Name <amandagxagxa@gmail.com>
2018-11-21 14:28:25 +0200

    binded page with vue

--- client/src/assets/js/devpool.js
+++ client/src/assets/js/devpool.js
@@ -1,15 +1,30 @@
-
 let devPool = new Vue({
   el: '.devPool',
-   data : {     
+  data: {
     // add this
     username: '',
     password: '',
-    checked :''
+    checked: ''
   },
-  methods : {
-    details : function() {
-      alert('It happenning!!!');
+  methods: {
+    error: function () {
+      // let password = /^[A-Z]\[a-z]\d{1,3}$/
+      if (this.username === '') {
+        console.log('you need a name')
+      } else if (this.password === '') {
+        console.log('plz enter password')
+      }
+      return '';
+    
+    submit: function (e) {
+      e.preventDefault();
+      alert('It happenning!!!')
+      axios.post('/api/user/register', {
+        username: this.username, password: this.password
+      })
+      .then(result => {
+       
+      }
     }
   }
-});
+})
