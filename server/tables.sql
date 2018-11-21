de8f23f4ed2e57f64d79df657977546d016ec2e1
Luvuyo Sono <luvuyosono@gmail.com>
2018-11-21 15:30:22 +0200

    database schema and registration logic

--- server/tables.sql
+++ server/tables.sql
@@ -1,10 +1,14 @@
+drop table if exists users;
 
 create table users (
     id serial not null primary key,
-    full_name varchar(55) not null,
-    email varchar(100) not null,
-    city varchar(55),
-    github varchar(55) not null,
-    codewars varchar(255) not null--,
-    -- linkedin not null varchar(255),
+    username varchar(50) not null, 
+    password varchar(100), 
+    full_name varchar(50) not null,
+    email varchar(50) not null, 
+    avatar varchar(255) not null, 
+    city varchar(55), 
+    active_since date not null,
+    updated_at date not null,
+    public_repos int not null default 0
 );
